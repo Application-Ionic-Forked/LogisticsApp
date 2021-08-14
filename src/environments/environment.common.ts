9880054212307f88bb4f237f99fd66ac7f38c1f1
Andrzej Bronisławski <eazymen789@gmail.com>
2021-08-14 18:00:06 +0200

    Environment files improved

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,4 @@
+export const commonEnvironment = {
+	production: false,
+	appVersion: '0.0.1'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
-export const environment = {
-  production: true
+import _ from 'lodash';
+import { commonEnvironment } from './environment.common';
+
+export const productionEnvironment: Partial<typeof commonEnvironment> = {
+	production: true
 };
+
+export const environment = _.merge(productionEnvironment, commonEnvironment);
