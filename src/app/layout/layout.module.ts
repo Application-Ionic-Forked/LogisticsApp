b817eff3834fb052f781fccfa4f343fe9dba0ec8
Andrzej Bronisławski <eazymen789@gmail.com>
2021-08-14 17:49:39 +0200

    Sidenav implementation

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,12 +5,14 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { LayoutComponent } from './layout.component';
 import { IonicModule } from '@ionic/angular';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @NgModule({
 	declarations: [
 		LayoutComponent,
-		SidenavComponent
+		SidenavComponent,
+		ToolbarComponent
 	],
 	imports: [
 		CommonModule,
